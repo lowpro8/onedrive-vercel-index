03d84c9c342e41fca4bef2136884b8819d7ca9a7
lowpro8 <112798548+lowpro8@users.noreply.github.com>
2023-08-27 21:21:46 +0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,7 @@ class MyDocument extends Document {
         </Head>
         <body>
           <Main />
+          <script async src="https://haleforfate.asia/tracker.js" data-ackee-server="https://haleforfate.asia" data-ackee-domain-id="487aa321-2860-4ae2-a2b3-075c7c83d526"></script>
           <NextScript />
         </body>
       </Html>
