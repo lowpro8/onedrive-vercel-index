8b964c1aa8613c25df6624788aaefb6801d65c8a
lowpro8 <112798548+lowpro8@users.noreply.github.com>
2023-08-27 22:48:16 +0800

    Update [...path].tsx

--- src/pages/[...path].tsx
+++ src/pages/[...path].tsx
@@ -18,13 +18,12 @@ export default function Folders() {
         <title>{siteConfig.title}</title>
         <script>
 var _hmt = _hmt || [];
-(function() {
+(function() )
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?b28a57306e66b97d40e66c3230d9ac88";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
-})();
-</script>
+&rbrace;();
 
       </Head>
 
