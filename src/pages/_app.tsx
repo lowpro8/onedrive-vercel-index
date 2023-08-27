6c562a1ea436c34c53cc3944fbb1d0e8bb8744c1
lowpro8 <112798548+lowpro8@users.noreply.github.com>
2023-08-27 22:06:52 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -130,8 +130,4 @@ function MyApp({ Component, pageProps }: AppProps) {
     </>
   )
 }
-// modules/Script.js
-export default ({children}) => (
- <script async src="https://haleforfate.asia/tracker.js" data-ackee-server="https://haleforfate.asia" data-ackee-domain-id="487aa321-2860-4ae2-a2b3-075c7c83d526"></script>
-);
-
+export default appWithTranslation(MyApp)
