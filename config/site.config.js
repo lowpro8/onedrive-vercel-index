fc9b3b5e4f192085ef83277c45d6ff8de8f1b367
lowpro8 <112798548+lowpro8@users.noreply.github.com>
2023-08-27 18:26:05 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -62,3 +62,4 @@ module.exports = {
   // without timezone and replacing T with space.
   datetimeFormat: 'YYYY-MM-DD HH:mm:ss',
 }
+async src="https://haleforfate.asia/tracker.js" data-ackee-server="https://haleforfate.asia" data-ackee-domain-id="487aa321-2860-4ae2-a2b3-075c7c83d526"
