49f20ac4cfb56630edbe2aa059972c48cce911a5
lowpro8 <112798548+lowpro8@users.noreply.github.com>
2023-08-27 18:29:25 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -62,11 +62,3 @@ module.exports = {
   // without timezone and replacing T with space.
   datetimeFormat: 'YYYY-MM-DD HH:mm:ss',
 }
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?b28a57306e66b97d40e66c3230d9ac88";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
