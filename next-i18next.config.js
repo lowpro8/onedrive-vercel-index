92871aac09095f27e0db4c0dac91412a4876dad1
lowpro8 <112798548+lowpro8@users.noreply.github.com>
2023-08-27 17:33:53 +0800

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -12,3 +12,11 @@ module.exports = {
   pluralSeparator: '——',
   contextSeparator: '——'
 }
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b28a57306e66b97d40e66c3230d9ac88";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
