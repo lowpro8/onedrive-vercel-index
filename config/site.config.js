96e1477d8a9a8101dbf6902d21b3e084f051e4c4
lowpro8 <112798548+lowpro8@users.noreply.github.com>
2023-08-29 19:42:11 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -43,7 +43,7 @@ module.exports = {
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes:[ 
-            '/Involving secrets'，
+            '/Involving secrets',
                   '/Download',
                    ],
 
