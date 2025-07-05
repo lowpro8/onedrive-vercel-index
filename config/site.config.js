3f4c152db0da90ad244987a08e55a85341bb6243
lowpro8 <112798548+lowpro8@users.noreply.github.com>
2025-07-05 18:12:44 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '此情可待成追忆,<a href="https://bluare-my.sharepoint.com/:f:/g/personal/tutuo_bluare_onmicrosoft_com/EjqmSPCbtdhDrcpANdYH2o0BU_6ntiRJrxiSnIZ24p_qdw?e=c7QaGZ" target="_blank" rel="noopener noreferrer">只是</a>当时已惘然.',
+    '此情可待成追忆,<a href="https://bluare-my.sharepoint.com/:f:/g/personal/tutuo_bluare_onmicrosoft_com/EvWSzbmxxdpGneu90JntFUsBFfrP5d10zoiMD8kFnYWz_g?e=liul1Q">只是</a>当时已惘然.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -68,7 +68,7 @@ module.exports = {
   links: [
     {
       name: 'Microsoft',
-      link: 'https://bluare-my.sharepoint.com/:f:/g/personal/tutuo_bluare_onmicrosoft_com/EjqmSPCbtdhDrcpANdYH2o0BU_6ntiRJrxiSnIZ24p_qdw?e=c7QaGZ',
+      link: 'https://bluare-my.sharepoint.com/:f:/g/personal/tutuo_bluare_onmicrosoft_com/EvWSzbmxxdpGneu90JntFUsBFfrP5d10zoiMD8kFnYWz_g?e=liul1Q',
     },
   ],
 
